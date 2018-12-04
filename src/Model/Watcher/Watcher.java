2e4e847c2d92ab49f2256bb2ab4cdbdc69cdb4d1
Prozengan <prozengan@gmail.com>
2018-12-04 21:39:03 +0100

    Horodatage lié à la connexion

--- src/Model/Watcher/Watcher.java
+++ src/Model/Watcher/Watcher.java
@@ -3,6 +3,7 @@ package Model.Watcher;
 import Model.EtudiantExamenInfoSingleton;
 import Model.ServerLinkSingleton;
 
+import java.sql.Time;
 import java.util.Date;
 
 import org.json.simple.JSONObject;
@@ -12,11 +13,13 @@ public abstract class Watcher extends Thread {
 	
 	// Le nom du type du Watcher
 	protected String type;
+	private long tempsDemarrage;
 
 	// CONSTRUCTOR
 
 	public Watcher(String type) {
 		this.type = type;
+		this.tempsDemarrage = System.currentTimeMillis();
 	}
 
 	/**
@@ -29,23 +32,40 @@ public abstract class Watcher extends Thread {
 
 	/**
 	 * Méthode qui sera défini dans les classes filles pour traiter et construire l'objet JSON qui sera envoyé
+	 * La transformation du long en string provient de : http://www.java2s.com/Code/Java/Development-Class/Takesatimeinmillisecondsandreturnsanhoursminutesandsecondsrepresentation.htm
 	 */
 	@SuppressWarnings("unchecked")
 	protected  void createDataBeforeSendEvent(String typ, String information)
 	{
-		Date date = new Date();
-		@SuppressWarnings("deprecation")
-		int hour = date.getHours();
-		@SuppressWarnings("deprecation")
-		int minute = date.getMinutes();
-		@SuppressWarnings("deprecation")
-		int sec = date.getSeconds();
+		long tempsEnvoi = System.currentTimeMillis();
+		long delai = tempsEnvoi - tempsDemarrage;
+		
+		long tempsSeconde = delai/1000;
+        long heure = (tempsSeconde / 3600);
+        long min = (tempsSeconde / 60) % 60;
+        long sec = tempsSeconde % 60;
+        String horodatage;
+        String minsString = (min == 0)
+            ? "00"
+            : ((min < 10)
+               ? "0" + min
+               : "" + min);
+        String secsString = (sec == 0)
+            ? "00"
+            : ((sec < 10)
+               ? "0" + sec
+               : "" + sec);
+        if (heure > 0)
+            horodatage =  "" + heure + ":" + minsString + ":" + secsString;
+        else if (min > 0)
+        	horodatage = "00:" + min + ":" + secsString;
+        else horodatage = "00:00:" + secsString;
 		
 		// Niveau d'alerte à ajouter et autres infos si besoin
 		JSONObject datas = new JSONObject();
 		datas.put("IDexamen", EtudiantExamenInfoSingleton.getInstanceExistante().getNumeroExamen());
 		datas.put("mailEtudiant", EtudiantExamenInfoSingleton.getInstanceExistante().getIdentifiant());
-		datas.put("horodatage", hour+":"+minute+":"+sec);
+		datas.put("horodatage", horodatage);
 		datas.put("type", typ);
 		datas.put("info", information);
 		
